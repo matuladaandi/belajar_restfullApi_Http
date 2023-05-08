e84225f025bb0034540dc910f3a2ba4d8f2a8d87
andi <echo.matuladaandi@gmail.com>
2023-05-08 14:19:10 +0800

    add file home_stw_future, start project get api stateless future builder

---
+++ lib/pages/home_stw_future.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePageStateLessFutureBuilder extends StatelessWidget {
+  const HomePageStateLessFutureBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Get Api Future Builder'),
+      ),
+    );
+  }
+}
